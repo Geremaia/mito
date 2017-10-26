8e425bf00d1989f8ed4aae791e46e22af81f8e98
Leonardo Gremia <Geremaia@localhost.localdomain>
2017-10-26 11:12:48 -0200

    Ajeitando crud users

---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'As senhas precisam ter ao menos 6 digitos e elas tem que ser iguais.',
+    'reset' => 'Sua senha foi resetada!',
+    'sent' => 'Nós enviamos um e-mail com um link para resetar sua senha!',
+    'token' => 'Este token de redefinição de senha é inválido..',
+    'user' => "Não encontramos um usuário que possua esse endereço e-mail.",
+
+];
