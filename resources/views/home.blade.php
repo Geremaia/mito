8a8a03a4dc7077c87677a639d42b9515a90c1c83
Leonardo Gremia <Geremaia@localhost.localdomain>
2017-11-05 10:35:03 -0200

    colocando opções

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    
+                    <p>Vocẽ está logado no sistema!</p>
                 </div>
             </div>
         </div>
