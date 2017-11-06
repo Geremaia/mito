da9e420829346c0ec18af266271a21a502b7716a
Leonardo Gremia <Geremaia@localhost.localdomain>
2017-11-06 21:01:14 -0200

    Ajeitando produtos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,35 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <p>Vocẽ está logado no sistema!</p>
+                    <div class="x_content">
+                    <table class="table">
+                      <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Ações</th>
+                      </tr>
+                      <?php foreach($products as $product): ?>
+                        <tr>
+                          <td><?= $product->getId() ?></td>
+                          <td><?= $product->getNome() ?></td>
+                          <td>R$ <?= $product->getValor() ?></td>
+                          <td><?= $product->getCategoria() ?></td>
+                          <td>
+                            <a href="/admin/products/show?id=<?=$product->getId()?>" class="btn btn-xs btn-primary">
+                              <i class="fa fa-eye"></i>
+                            </a>
+                            <a href="/admin/products/edit?id=<?=$product->getId()?>" class="btn btn-xs btn-warning">
+                              <i class="fa fa-pencil"></i>
+                            </a>
+                            <a href="/admin/products/delete?id=<?=$product->getId()?>" class="btn btn-xs btn-danger">
+                              <i class="fa fa-trash"></i>
+                            </a>
+                          </td>
+                        </tr>
+                      <?php endforeach; ?>
+                    </table>
                 </div>
             </div>
         </div>
