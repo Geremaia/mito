fd737620976e62473c9dd4e93590950dfa8413d5
Leonardo Gremia <Geremaia@localhost.localdomain>
2017-11-07 06:46:42 -0200

    não deu

--- resources/views/products/delete.blade.php
+++ resources/views/products/delete.blade.php
@@ -16,12 +16,7 @@
                         <label for="name" class="col-md-4 control-label">ID</label>
 
                         <div class="col-md-6">
-                            <input type="text" id="product-id" name="product_id" required="required" class="form-control col-md-7 col-xs-12" readonly>
-                            <script> 
-                                var url = document.URL;
-                                var id = url.substring(url.lastIndexOf('=') + 1);
-                                document.getElementById("product-id").value = id; 
-                            </script>
+                            <input type="text" id="product-id" name="product_id" value="<?php$request->route('id');?>" required="required" class="form-control col-md-7 col-xs-12" readonly>
                         </div>
                     </div>
                     <div class="form-group">
